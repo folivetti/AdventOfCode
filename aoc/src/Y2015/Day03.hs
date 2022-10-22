e29c86376956af2fc06a9ef4f23318dc82833be5
folivetti <folivetti@ufabc.edu.br>
2022-10-22 16:00:02 +0200

    day 3 - 2015

--- aoc/src/Y2015/Day03.hs
+++ aoc/src/Y2015/Day03.hs
@@ -1,20 +1,24 @@
 module Y2015.Day03 (solution) where
 
-import qualified Data.Map.Strict as M
+import qualified Data.Set as S
 
-makeMove :: Char -> (Int, Int) -> (Int, Int)
-makeMove '>' (x, y) = (x+1, y)
-makeMove '<' (x, y) = (x-1, y)
-makeMove '^' (x, y) = (x, y+1)
-makeMove 'v' (x, y) = (x, y-1)
+makeMove :: (Int, Int) -> Char -> (Int, Int)
+makeMove (x, y) '>' = (x+1, y)
+makeMove (x, y) '<' = (x-1, y)
+makeMove (x, y) '^' = (x, y+1)
+makeMove (x, y) 'v' = (x, y-1)
 makeMove _ _ = error "invalid char"
+
+makeMoveAndSwap :: [(Int, Int)] -> Char -> [(Int, Int)]
+makeMoveAndSwap [santa, other] m = [other, makeMove santa m]
+makeMoveAndSwap _ _ = error "wrong list"
 
 solution :: IO ()
 solution = do
   content <- readFile "inputs/2015/input03.txt"
   let
-    santa0 = ((0,0), 1)
-    part1 = M.size . M.fromListWith (+) . scanl (\(pos, _) m -> (makeMove m pos, 1)) santa0
-    part2 = M.size . M.fromListWith (+) . mconcat . scanl (\[(pos, _), santa] m -> [santa, (makeMove m pos, 1)]) [santa0, santa0]
+    santa0 = (0,0)
+    part1 = S.size . S.fromList . scanl makeMove santa0
+    part2 = S.size . S.fromList . mconcat . scanl makeMoveAndSwap [santa0, santa0]
   print $ part1 content
   print $ part2 content
