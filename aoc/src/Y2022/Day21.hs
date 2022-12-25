a7e91ba59261055169d6aff3f141daf48dde6e7c
folivetti <folivetti@ufabc.edu.br>
2022-12-25 06:59:12 -0300

    day 22

--- aoc/src/Y2022/Day21.hs
+++ aoc/src/Y2022/Day21.hs
@@ -1,4 +1,72 @@
+{-# language OverloadedStrings #-}
 module Main where
 
+import Data.Attoparsec.ByteString.Char8
+import qualified Data.ByteString.Char8 as B
+import Utils ( runParser )
+import Control.Applicative ( (<|>) )
+import qualified Data.Map as M
+
+type Value = Either Double (String, String, Char)
+
+parser :: Parser (M.Map String Value)
+parser = do key <- manyTill anyChar (string ": ")
+            val <- eitherP (fromInteger <$> decimal) ops
+            pure $ M.singleton key val
+  where
+    ops = operator '+' <|> operator '-' <|> operator '*' <|> operator '/'
+    operator c = do l <- manyTill anyChar (char ' ')
+                    o <- anyChar
+                    char ' '
+                    r <- many' anyChar
+                    pure (l, r, o)
+
+evalOp :: Char -> Double -> Double -> Double
+evalOp '+' = (+)
+evalOp '-' = (-)
+evalOp '*' = (*)
+evalOp '/' = (/)
+evalOp  _  = undefined
+
+evalTree :: M.Map String Value -> Double
+evalTree tree = go (tree M.! "root")
+  where
+    go (Left  n)          = n
+    go (Right (l, r, op)) = evalOp op (go (tree M.! l)) (go (tree M.! r))
+
+solve :: M.Map String Value -> Double
+solve tree = fromLeft $ go "root"
+  where
+    fromLeft (Left x) = x
+    invOpR '+' x = subtract x
+    invOpR '*' x = (/ x)
+    invOpR '-' x = (+x)
+    invOpR '/' x = (*x)
+    
+    invOpL '+' x = subtract x
+    invOpL '*' x = (/ x)
+    invOpL '-' x = negate . subtract x
+    invOpL '/' x = recip . (/ x)
+    
+    go "root" =
+        let Right (l, r, op) = tree M.! "root"
+        in case (go l, go r) of
+             (Left x, Right f) -> Left $ f x
+             (Right f, Left x) -> Left $ f x
+             _                 -> undefined
+    go "humn" = Right id
+    go node =
+        case tree M.! node of
+          Left n -> Left n
+          Right (l, r, op) -> case (go l, go r) of
+                                (Left x, Left y) -> Left (evalOp op x y)
+                                (Right f, Left y) -> Right (f . invOpR op y)
+                                (Left x, Right f) -> Right (f . invOpL op x)
+                                _                 -> undefined
+
 main :: IO ()
-main = undefined
+main = do content <- M.unions . map (runParser parser) . B.lines <$> B.readFile "inputs/2022/input21.txt"
+          let Right (l, r, op) = content M.! "root"
+              content' = M.insert "root" (Right (l, r, '-')) content
+          print $ round $ evalTree content
+          print $ round $ solve content'
--- aoc/src/Y2022/Day22.hs
+++ aoc/src/Y2022/Day22.hs
@@ -1,4 +1,99 @@
+{-# language ViewPatterns #-}
+{-# language OverloadedStrings #-}
+{-# language TupleSections #-}
 module Main where
 
+import Data.Attoparsec.ByteString.Char8
+import qualified Data.ByteString.Char8 as B
+import qualified Data.Map as M
+import Control.Arrow ( (&&&) )
+import Data.List ( transpose )
+import Utils ( runParser )
+import Debug.Trace ( trace )
+
+type Coord = (Int, Int)
+type Entries = M.Map Int Coord
+data Dir = N | S | E | W deriving (Show)
+
+rot :: Char -> Dir -> Dir
+rot 'R' N = E
+rot 'R' E = S
+rot 'R' S = W
+rot 'R' W = N
+rot 'L' N = W
+rot 'L' W = S
+rot 'L' S = E
+rot 'L' E = N
+rot _ _ = error "wrong rot"
+
+enumerate :: [a] -> [(Int, a)]
+enumerate = zip [0..]
+
+getColumns :: [Char] -> [(Int, Char)]
+getColumns = filter ((`elem` (".#" :: String)) . snd) . enumerate
+
+parseInstructions :: Parser [Either Int Char]
+parseInstructions = many' $ eitherP decimal anyChar
+
+parseMap :: [String] -> (M.Map Coord Char, [Either Int Char])
+parseMap = cube &&& (runParser parseInstructions . B.pack . last)
+  where
+    cube             = parseMap . init . init
+    parseMap         = M.unions . map parseRow . enumerate . map getColumns
+    parseRow (i, ys) = M.fromList . map (\(j,c) -> ((i,j), c)) $ ys
+
+entries :: [String] -> (Entries, Entries)
+entries (init . init -> xs) = (entriesRows &&& entriesCols) xs
+  where
+    n                  = length xs
+    entriesRows        = M.unions . map entriesRow . enumerate . map getColumns
+    entriesRow (i, ys) = M.singleton i . (head &&& last) . map fst $ ys
+    adjustIx (i, c)    = (i,c) -- (n-i-1, c)
+    swap (a, b)        = (b, a)
+    entriesCols        = M.unions . map entriesRow . enumerate . map (map adjustIx . getColumns) . transpose
+
+walk :: M.Map Coord Char -> (Entries, Entries) -> Dir -> Int -> Coord -> Coord
+walk cube (eRows, eCols) dir n = go n
+  where
+    getWarp E (x, y) = (x,) . fst $ eRows M.! x
+    getWarp W (x, y) = (x,) . snd $ eRows M.! x
+    getWarp S (x, y) = (,y) . fst $ eCols M.! y
+    getWarp N (x, y) = (,y) . snd $ eCols M.! y
+
+    addDir E (x, y) = (x, y+1)
+    addDir W (x, y) = (x, y-1)
+    addDir S (x, y) = (x+1, y)
+    addDir N (x, y) = (x-1, y)
+    
+    go 0 xy = xy
+    go m xy = case cube M.!? xy' of
+                Nothing -> let warped = getWarp dir xy 
+                            in trace (show (xy,dir,xy',warped)) $ if cube M.! warped == '.'
+                                 then go (m-1) warped
+                                 else xy
+                Just '.' -> go (m-1) xy'
+                Just '#' -> xy
+                _        -> error "where are you going, mate?"
+      where
+        xy' = addDir dir xy
+
+follow :: (Dir -> Int -> Coord -> Coord) -> (Coord, Dir) -> Either Int Char -> (Coord, Dir)
+follow walker (coord, dir) (Left n) = (,dir) $ walker dir n coord
+follow walker (coord, dir) (Right r) = (coord, ) $ rot r dir
+
+part1 ((x,y), dir) = 1000 * (x + 1) + 4 * (y + 1) + cost dir
+  where
+      cost E = 0
+      cost S = 1
+      cost W = 2
+      cost N = 3
+
 main :: IO ()
-main = undefined
+main = do content <- lines <$> readFile "inputs/2022/input22.txt"
+          let (cube, instructions) = parseMap content
+              (eRows, eCols) = entries content
+              walker = walk cube (eRows, eCols)
+              s0 = (0,) $ fst $ eRows M.! 0
+          print eRows
+          print eCols
+          print $ part1 $ foldl (follow walker) (s0,E) instructions
