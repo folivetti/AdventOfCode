6b4be3279d0a63ba3768e7da7d9b2ae3c52afe28
folivetti <folivetti@ufabc.edu.br>
2022-12-26 14:08:28 -0300

    pqueue day 24

--- aoc/src/Y2022/Day24.hs
+++ aoc/src/Y2022/Day24.hs
@@ -4,6 +4,7 @@ module Main where
 import qualified Data.Map as M
 import qualified Data.Set as S
 import Data.List ( find, sortOn )
+import qualified Data.OrdPSQ as P
 
 type Valley = M.Map (Int, Int) String
 
@@ -32,26 +33,31 @@ updateValley (maxX, maxY) valley = M.fromListWith (<>) $ concatMap (uncurry upd)
     move (x, y) 'v' = (((x + 1) `mod` maxX, y), "v")
 
 astar :: (Int, Int) -> M.Map Int Valley -> ((Int, Int), Int) -> (Int, Int) -> Int
-astar (maxX, maxY) valleys start end = go S.empty [start]
+astar (maxX, maxY) valleys start end = go S.empty $ P.singleton start (heur start) start
   where
-    go _ [] = error "all invalid states"
-    go seen (st@((x, y), t):xs)
-      | (x, y) == end      = t
-      | st `S.member` seen = go seen xs
-      | otherwise          = go (S.insert ((x,y), t) seen) xs'
+    getCyc t = t `mod` lcm maxX maxY
+    heur ((x,y), t') = t' + abs (x - fst end) + abs (y - snd end) 
+
+    go seen st
+      | cur == end                = t
+      | (cur, t) `S.member` seen  = go seen xs
+      | otherwise                 = go (S.insert (cur, t) seen) xs'
       where 
-        xs'              = sortOn heur (xs <> map (,t+1) (next (x,y)))
-        cyc              = (t + 1) `mod` lcm maxY maxX
-        valley           = valleys M.! cyc
-        next (a, b)      = filter valid [(a+1, b), (a-1, b), (a, b+1), (a, b-1), (a, b)]
-        inBound (a, b)   = (a, b) == fst start || (a, b) == end || (a >= 0 && a < maxX && b >= 0 && b < maxY)
-        valid (a, b)     = ((a, b), cyc) `S.notMember` seen && inBound (a, b) && (a, b) `M.notMember` valley
-        heur ((a,b), t') = t' + abs (a - fst end) + abs (b - snd end)
+        (Just ((cur, t), _, _, xs)) = P.minView st
+        cyc                 = getCyc (t + 1)
+        valley              = valleys M.! cyc
+        xs'                 = foldr ((\x acc -> P.insert x (heur x) x acc) . (,t+1)) xs $ next cur
+        next (a, b)         = filter valid [(a+1, b), (a-1, b), (a, b+1), (a, b-1), (a, b)]
+        inBound (a, b  )    = (a, b) == fst start || (a, b) == end || (a >= 0 && a < maxX && b >= 0 && b < maxY)
+        valid xy            =  (xy, cyc) `S.notMember` seen 
+                            && (xy, t+1) `S.notMember` seen
+                            && inBound xy 
+                            && xy `M.notMember` valley
 
 main :: IO ()
 main = do valley <- parseValley <$> readFile "inputs/2022/input24.txt"
           let valleySize   = (\(x,y) -> (x+1, y+1)) $ maximum $ M.keys valley
-              allValleys   = M.fromList $ enumerate $ take (uncurry lcm $ valleySize) $ iterate (updateValley valleySize) valley
+              allValleys   = M.fromList $ enumerate $ take (uncurry lcm valleySize) $ iterate (updateValley valleySize) valley
               (start, end) = ((-1, 0), (fst valleySize, snd valleySize - 1))
               t1 = astar valleySize allValleys (start, 0) end
               t2 = astar valleySize allValleys (end, t1) start
