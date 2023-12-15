561c4e1243e6148f1cc407de9151f533c2aa1211
folivetti <fabricio.olivetti@gmail.com>
2023-12-15 07:50:25 -0300

    day 15

---
+++ aoc/src/Y2023/Day15.hs
@@ -0,0 +1,48 @@
+module Main ( main ) where 
+
+import Rec
+import Data.List.Split ( splitOn )
+import Data.Char ( ord )
+import Data.List ( find )
+import qualified Data.Map.Strict as Map 
+
+hash = cata alg . fromList . reverse
+  where 
+    alg NilF = 0 
+    alg (ConsF x xs) =  ((ord x + xs) * 17) `rem` 256
+
+solve = cata alg . fromList . reverse
+  where 
+    alg NilF         = 0
+    alg (ConsF x xs) = hash x + xs
+
+getLabel = takeWhile (\x -> x /= '=' && x /= '-')
+
+getValue :: String -> Int 
+getValue = read . tail . dropWhile (/= '=')
+
+insertOrReplace (label, value) xs = 
+    case find ((==label) . fst) xs of 
+      Nothing -> xs <> [(label, value)]
+      Just _  -> map (\(lbl, val) -> if lbl==label then (lbl, value) else (lbl, val)) xs
+
+calcSlots = sum . zipWith (\ix (_,v) -> ix*v) [1..] 
+calcLens  = sum . map (\(ix, xs) -> (ix+1) * calcSlots xs) 
+
+solve2 = calcLens . filter (not . null . snd) . Map.toAscList . cata alg . fromList . reverse
+  where 
+    alg NilF = Map.empty 
+    alg (ConsF x xs)
+      | '=' `elem` x = Map.insert lenId (insertOrReplace (label, value) lens) xs
+      | '-' `elem` x = if null lens then xs else Map.insert lenId (filter ((/=label) . fst) lens) xs
+      where 
+        label = getLabel x
+        value = getValue x
+        lenId = hash label
+        lens  = Map.findWithDefault [] lenId xs
+
+                           
+
+main :: IO ()
+main = solve2 . splitOn "," . filter (/='\n') <$> readFile "inputs/2023/input15.txt"
+         >>= print
