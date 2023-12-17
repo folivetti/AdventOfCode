5461690f0b1c5cf51887bbb0b104d2b761b8eafb
folivetti <fabricio.olivetti@gmail.com>
2023-12-17 13:49:16 -0300

    day 17

--- aoc/src/Y2023/Day16.hs
+++ aoc/src/Y2023/Day16.hs
@@ -7,48 +7,56 @@ import qualified Data.Set as Set
 import qualified Data.Map.Strict as Map
 import Control.Arrow ( (&&&) ) 
 import Control.Parallel.Strategies ( parMap, rpar )
+import Data.Semigroup ( Max(..) )
 
-data Coord = C Int Int deriving (Show, Eq, Ord)
+rot90, rotm90 :: (Int, Int) -> (Int, Int)
+rot90 (x, y)      = (y, x)
+{-# INLINE rot90 #-}
+rotm90 (x, y)     = (-y, -x)
+{-# INLINE rotm90 #-}
+(.+.) :: (Int, Int) -> (Int, Int) -> (Int, Int)
+(x, y) .+. (a, b) = (x+a, y+b)
+{-# INLINE (.+.) #-}
 
-instance Num Coord where 
-    (C x y) + (C z w) = C (x+z) (y+w)
-    (C x y) - (C z w) = C (x-z) (y-w)
-    (C x y) * (C z w) = C (x*z) (y*w)
-    abs (C x y)       = C (abs x) (abs y)
-    signum (C x y)    = 0
-    fromInteger x     = C (fromInteger x) (fromInteger x)
-    negate (C x y)    = C (negate x) (negate y)
-    
-toC (x, y)   = C x y
-getX (C x _) = x 
-getY (C _ y) = y
-swap (C x y) = C y x
+east, west, north, south :: (Int, Int)
+east  = (0, 1)
+west  = (0, -1)
+north = (-1, 0)
+south = (1, 0)
 
-s0s = concat [ [(C 0 x, C 1 0), (C x 0, C 0 1), (C 109 x, C (-1) 0), (C x 109, C 0 (-1))] | x <- [0 .. 109] ]
+s0s :: [[((Int, Int), (Int, Int))]]
+s0s = [ [((0, x), south), ((x, 0), east), ((109, x), north), ((x, 109), west)] | x <- [0 .. 109] ]
 
+solve :: ((Int, Int), (Int, Int)) -> Map.Map (Int, Int) Char -> Int
 solve s0 contraption = Set.size $ hylo alg coalg ([s0], Set.singleton s0)
   where
     alg NilF           = Set.empty
-    alg (ConsF c cs)   = Set.fromList c <> cs
+    alg (ConsF c cs)   = Set.union c cs
 
     coalg ([], _) = NilF 
-    coalg (candidates, seen) = let candidates' = filter ((`Map.member` contraption) . fst) . filter (not . (`Set.member` seen)) $ concatMap step candidates
+    coalg (candidates, seen) = let candidates' = foldr (step (valid seen)) [] candidates
                                    seen'       = Set.fromList candidates' <> seen
-                                in ConsF (map fst candidates) (candidates', seen')
+                                in ConsF (Set.fromList $ map fst candidates) (candidates', seen')
 
-    step (coord, dir) =
+    valid seen (c, d) = (c, d) `Set.notMember` seen && c `Map.member` contraption
+
+    step p (coord, dir) acc =
         case contraption Map.!? coord of 
-          Nothing -> []
-          Just '|' | getX dir == 0 -> [(coord + C (-1) 0, C (-1) 0), (coord + C 1 0, C 1 0)]
-          Just '-' | getY dir == 0 -> [(coord + C 0 (-1), C 0 (-1)), (coord + C 0 1, C 0 1)]
-          Just '/' -> let dir' = negate (swap dir) in [(coord + dir', dir')]
-          Just '\\' -> let dir' = swap dir in [(coord + dir', dir')]
-          _ -> [(coord + dir, dir)]
+          Nothing -> acc
+          Just '|' | fst dir == 0 -> [(coord .+. d, d) | d <- [north, south], p (coord .+. d, d)] <> acc 
+          Just '-' | snd dir == 0 -> [(coord .+. d, d) | d <- [west, east], p (coord .+. d, d)] <> acc
+          Just '/'                -> let dir' = rotm90 dir 
+                                         pt   = (coord .+. dir', dir')
+                                      in if p pt then pt : acc else acc 
+          Just '\\'               -> let dir' = rot90 dir 
+                                         pt   = (coord .+. dir', dir')
+                                      in if p pt then pt : acc else acc
+          _                       -> let pt = (coord .+. dir, dir) 
+                                      in if p pt then pt : acc else acc
 
-
-solve1    = solve (C 0 0, C 0 1)
-solve2 xs = maximum $ parMap rpar (`solve` xs) s0s
+solve1    = solve ((0, 0), east)
+solve2 xs = maximum $ parMap rpar (foldMap (Max . (`solve` xs))) s0s
 
 main :: IO ()
-main = (solve1 &&& solve2) . Map.fromList . map (\(c,x) -> (toC c, x)) . toCoord . lines <$> readFile "inputs/2023/input16.txt"
+main = (solve1 &&& solve2) . Map.fromList . toCoord . lines <$> readFile "inputs/2023/input16.txt"
          >>= print
